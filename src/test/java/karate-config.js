db1c708021386bcfead803c3665ee0bdbb0a1c62
Jessnar_Sinues <Jessnar_Sinues@discovery.com>
2021-03-23 14:14:53 +0800

    Updated karate-config.js

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -10,25 +10,16 @@ function() {
   var envData = read('classpath:env.json');
   var envConfig = envData[targetEnv];
   
-  // var secretsData = null;
-  // try {
-  //   var AWSUtilsClass = Java.type('AWSUtils.AWSUtils');
-  //   var AWSUtils = new AWSUtilsClass();
-  //   secretsData = AWSUtils.getSecrets(envConfig.Common.SecretsManager.secretName, envConfig.Common.SecretsManager.region);
-  // } catch (err) {
-  //   karate.log('Problem encountered while fetching data from Secrets Manager');
-  //   karate.log(err);
-  //   karate.abort();
-  // }
-  
-  var secretsData = {
-    "Iconik-AdminEmailqa": "rohan_mundkur@discovery.com",
-    "Iconik-AdminPasswordqa": "*aeWV56gmrEPMi",
-    "Iconik-AdminEmailpreprod": "iconik-prod-admin@discovery.com",
-    "Iconik-AdminPasswordpreprod": "4z9nxyy9vpf0hw8l",
-    "Iconik-AdminEmailprod": "iconik-prod-admin@discovery.com",
-    "Iconik-AdminPasswordprod": "4z9nxyy9vpf0hw8l"
-  };
+  var secretsData = null;
+  try {
+    var AWSUtilsClass = Java.type('AWSUtils.AWSUtils');
+    var AWSUtils = new AWSUtilsClass();
+    secretsData = AWSUtils.getSecrets(envConfig.Common.SecretsManager.secretName, envConfig.Common.SecretsManager.region);
+  } catch (err) {
+    karate.log('Problem encountered while fetching data from Secrets Manager');
+    karate.log(err);
+    karate.abort();
+  }
 
   var config = {
     TargetEnv: targetEnv,
